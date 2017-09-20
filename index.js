7bddc8ea8be9d9f597bb627a6c258ad96eaa93fa
= <terrahen@web.de>
2017-09-20 20:48:51 +0200

    load local config

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 const core = require('tzel-core');
-const settings = require('./tzel.json');
+
+let settings = null;
+if (fs.existsSync('./install/configs/configs.json')) {
+  settings = require('./install/configs/configs.json');
+} else {
+  settings = require('./tzel.json');
+}
 
 settings.require = require.bind();
 settings.root = __dirname;
